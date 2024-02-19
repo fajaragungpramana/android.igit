dd758d623f6a974106fa375e40ed5fb181564c06
fajaragungpramana <fajar.agungpramana01@gmail.com>
2024-02-19 23:54:40 +0700

    Remove unused extension.

--- app/src/main/java/com/github/fajaragungpramana/igit/extension/ContextExtension.kt
+++
@@ -1,9 +0,0 @@
-package com.github.fajaragungpramana.igit.extension
-
-import android.content.Context
-import android.content.Intent
-
-inline fun <reified T> Context.startActivity(vararg data: Pair<String, Any>) {
-    val i = Intent(this, T::class.java)
-    startActivity(i)
-}
