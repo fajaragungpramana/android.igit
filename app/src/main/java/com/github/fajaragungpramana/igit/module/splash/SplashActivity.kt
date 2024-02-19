0fb50724d6f6a579c648ff21c9b196867cb3e3c0
fajaragungpramana <fajar.agungpramana01@gmail.com>
2024-02-19 23:47:26 +0700

    Add animation on dismiss splash.

--- app/src/main/java/com/github/fajaragungpramana/igit/module/splash/SplashActivity.kt
+++ app/src/main/java/com/github/fajaragungpramana/igit/module/splash/SplashActivity.kt
@@ -1,8 +1,12 @@
 package com.github.fajaragungpramana.igit.module.splash
 
+import android.animation.ObjectAnimator
 import android.annotation.SuppressLint
 import android.os.Bundle
 import android.os.Handler
+import android.view.View
+import android.view.animation.AnticipateInterpolator
+import androidx.core.animation.doOnEnd
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.github.fajaragungpramana.igit.common.app.AppActivity
 import com.github.fajaragungpramana.igit.databinding.ActivitySplashBinding
@@ -12,17 +16,30 @@ class SplashActivity : AppActivity<ActivitySplashBinding>() {
 
     private var keep = true
 
-    override fun onViewBinding(): ActivitySplashBinding =
-        ActivitySplashBinding.inflate(layoutInflater)
-
-    override fun onCreated(savedInstanceState: Bundle?) {
+    override fun onViewBinding(): ActivitySplashBinding {
         installSplashScreen().let {
             it.setKeepOnScreenCondition { keep }
-            it.setOnExitAnimationListener {
+            it.setOnExitAnimationListener { viewProvider ->
 
+                val slideUp = ObjectAnimator.ofFloat(
+                    viewProvider.view,
+                    View.TRANSLATION_Y,
+                    0f,
+                    -viewProvider.view.height.toFloat()
+                )
+                slideUp.interpolator = AnticipateInterpolator()
+                slideUp.duration = 200L
+
+                slideUp.doOnEnd { viewProvider.remove() }
+
+                slideUp.start()
             }
         }
 
+        return ActivitySplashBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreated(savedInstanceState: Bundle?) {
         Handler(mainLooper).postDelayed({ keep = false }, DELAY)
     }
 
