187d99c350b80e96e0efc704baaa858ccb2cb9cd
fajaragungpramana <fajar.agungpramana01@gmail.com>
2024-02-20 18:53:20 +0700

    Create state and event for get list user.

--- app/src/main/java/com/github/fajaragungpramana/igit/module/search/SearchFragment.kt
+++ app/src/main/java/com/github/fajaragungpramana/igit/module/search/SearchFragment.kt
@@ -1,17 +1,37 @@
 package com.github.fajaragungpramana.igit.module.search
 
 import android.os.Bundle
+import android.util.Log
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import com.github.fajaragungpramana.igit.common.app.AppFragment
+import com.github.fajaragungpramana.igit.common.contract.AppState
 import com.github.fajaragungpramana.igit.databinding.SearchFragmentBinding
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 
-class SearchFragment : AppFragment<SearchFragmentBinding>() {
+@AndroidEntryPoint
+class SearchFragment : AppFragment<SearchFragmentBinding>(), AppState {
+
+    private val viewModel: SearchViewModel by viewModels()
 
     override fun onViewBinding(container: ViewGroup?): SearchFragmentBinding =
         SearchFragmentBinding.inflate(layoutInflater, container, false)
 
     override fun onViewCreated(savedInstanceState: Bundle?) {
+        viewModel.setEvent(SearchEvent.SearchUser("fajaragungpramana"))
+    }
 
+    override fun onStateObserver() {
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewModel.state.collectLatest {
+                when (it) {
+                    is SearchState.UserData -> Log.d("FFFF", "${it.pagingData}")
+                }
+            }
+        }
     }
 
 }
