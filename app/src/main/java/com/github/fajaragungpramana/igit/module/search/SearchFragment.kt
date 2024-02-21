b18193efd1f9f7e9a588bcdaffb9601388e276fc
fajaragungpramana <fajar.agungpramana01@gmail.com>
2024-02-21 15:26:12 +0700

    Create shimmering before load data user for the first time.

--- app/src/main/java/com/github/fajaragungpramana/igit/module/search/SearchFragment.kt
+++ app/src/main/java/com/github/fajaragungpramana/igit/module/search/SearchFragment.kt
@@ -4,13 +4,17 @@ import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
+import androidx.paging.LoadState
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.github.fajaragungpramana.igit.common.app.AppFragment
 import com.github.fajaragungpramana.igit.common.contract.AppState
 import com.github.fajaragungpramana.igit.databinding.SearchFragmentBinding
+import com.github.fajaragungpramana.igit.databinding.ShimmerItemUserBinding
+import com.github.fajaragungpramana.igit.extension.setMargins
 import com.github.fajaragungpramana.igit.module.adapter.LoadStateAdapter
 import com.github.fajaragungpramana.igit.module.adapter.UserAdapter
 import dagger.hilt.android.AndroidEntryPoint
@@ -30,6 +34,7 @@ class SearchFragment : AppFragment<SearchFragmentBinding>(), AppState {
     override fun onViewCreated(savedInstanceState: Bundle?) {
         initUser()
         initSearch()
+        initUserLoadState()
     }
 
     override fun onStateObserver() {
@@ -42,9 +47,27 @@ class SearchFragment : AppFragment<SearchFragmentBinding>(), AppState {
         }
     }
 
+    private fun initUserLoadState() {
+        userAdapter.addLoadStateListener {
+            val isLoading = it.refresh is LoadState.Loading
+            viewBinding.sflShimmerItemUser.isVisible = isLoading
+
+            if (isLoading) {
+                viewBinding.sflShimmerItemUser.startShimmer()
+
+                for (i in 1..5) {
+                    val shimmerItemUser = ShimmerItemUserBinding.inflate(layoutInflater).root
+                    viewBinding.llShimmerItemUser.addView(shimmerItemUser)
+
+                    shimmerItemUser.setMargins(16f, 0f, 16f, 16f)
+                }
+            } else
+                viewBinding.sflShimmerItemUser.stopShimmer()
+        }
+    }
+
     private fun initSearch() {
         viewModel.setEvent(SearchEvent.SearchUser(username = "a"))
-
         viewBinding.tieSearchUsername.addTextChangedListener {
             Handler(Looper.getMainLooper()).postDelayed({
                 viewModel.setEvent(SearchEvent.SearchUser(it.toString()))
