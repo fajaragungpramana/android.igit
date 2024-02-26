1aa80ce7397acbeb4adb49c1364b3c8ac06089a2
fajaragungpramana <fajar.agungpramana01@gmail.com>
2024-02-26 15:56:48 +0700

    Remove unused fragment.

--- app/src/main/java/com/github/fajaragungpramana/igit/module/popularity/PopularityEvent.kt
+++
@@ -1,9 +0,0 @@
-package com.github.fajaragungpramana.igit.module.popularity
-
-import com.github.fajaragungpramana.igit.core.data.remote.user.request.UserRequest
-
-sealed class PopularityEvent {
-
-    data class ListUser(val userRequest: UserRequest) : PopularityEvent()
-
-}
