f884a7fef4674d9bef3288ea7f633ae029e1c9ce
fajaragungpramana <fajar.agungpramana01@gmail.com>
2024-03-05 16:23:30 +0700

    Show list setting.

---
+++ app/src/main/java/com/github/fajaragungpramana/igit/module/adapter/SettingAdapter.kt
@@ -0,0 +1,28 @@
+package com.github.fajaragungpramana.igit.module.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.github.fajaragungpramana.igit.common.app.AppListAdapter
+import com.github.fajaragungpramana.igit.common.app.AppRecyclerViewHolder
+import com.github.fajaragungpramana.igit.core.domain.local.model.Setting
+import com.github.fajaragungpramana.igit.databinding.ItemSettingBinding
+
+class SettingAdapter :
+    AppListAdapter<ItemSettingBinding, Setting, SettingAdapter.ViewHolder>(Setting.diffUtil) {
+
+    override fun onViewBinding(viewGroup: ViewGroup): ItemSettingBinding =
+        ItemSettingBinding.inflate(LayoutInflater.from(viewGroup.context), viewGroup, false)
+
+    override fun onViewHolder(itemView: View): ViewHolder = ViewHolder(itemView)
+
+    inner class ViewHolder(itemView: View) : AppRecyclerViewHolder<Setting>(itemView) {
+
+        override fun bindItem(item: Setting, position: Int) {
+            viewBinding.mtvSettingTitle.text = item.title
+            viewBinding.mtvSettingOverview.text = item.overview
+        }
+
+    }
+
+}
--- app/src/main/java/com/github/fajaragungpramana/igit/module/setting/SettingFragment.kt
+++ app/src/main/java/com/github/fajaragungpramana/igit/module/setting/SettingFragment.kt
@@ -4,11 +4,13 @@ import android.os.Bundle
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.github.fajaragungpramana.igit.R
 import com.github.fajaragungpramana.igit.common.app.AppFragment
 import com.github.fajaragungpramana.igit.common.contract.AppState
 import com.github.fajaragungpramana.igit.core.data.local.sql.entity.SettingEntity
 import com.github.fajaragungpramana.igit.databinding.FragmentSettingBinding
+import com.github.fajaragungpramana.igit.module.adapter.SettingAdapter
 import com.github.fajaragungpramana.igit.module.main.MainActivity
 import com.google.android.material.snackbar.Snackbar
 import dagger.hilt.android.AndroidEntryPoint
@@ -20,11 +22,14 @@ class SettingFragment : AppFragment<FragmentSettingBinding>(), AppState {
 
     private val viewModel: SettingViewModel by viewModels()
 
+    private lateinit var settingAdapter: SettingAdapter
+
     override fun onViewBinding(container: ViewGroup?): FragmentSettingBinding =
         FragmentSettingBinding.inflate(layoutInflater)
 
     override fun onViewCreated(savedInstanceState: Bundle?) {
         initView()
+        initSetting()
 
         viewModel.setEvent(SettingEvent.Setting)
     }
@@ -32,6 +37,14 @@ class SettingFragment : AppFragment<FragmentSettingBinding>(), AppState {
     private fun initView() {
         (requireActivity() as MainActivity).apply {
             title = getString(R.string.settings)
+        }
+    }
+
+    private fun initSetting() {
+        settingAdapter = SettingAdapter()
+        viewBinding.rvSettings.apply {
+            layoutManager = LinearLayoutManager(requireActivity())
+            adapter = settingAdapter
         }
     }
 
@@ -51,6 +64,8 @@ class SettingFragment : AppFragment<FragmentSettingBinding>(), AppState {
                                     )
                                 )
                             )
+                        else
+                            settingAdapter.submitList(it.listSetting)
                     }
 
                     is SettingState.MessageData ->
